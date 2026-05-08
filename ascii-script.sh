747d752a462128defb9369559b0de8eb26866d8f
Srihari Kodati <90450520+srihari-kodati@users.noreply.github.com>
2026-05-08 12:05:31 +0530

    Update ascii-script.sh

--- ascii-script.sh
+++ ascii-script.sh
@@ -1,7 +1,7 @@
 #/bin/sh
 sudo apt-get install cowsay -y
 sudo apt install cowsay cowsay-off
-cowsay -f ./dragon.cow "Roar!" >> dragon.txt
+cowsay -f ./dragon.cow "DRAGON...Roar!!!" >> dragon.txt
 grep -i "dragon" dragon.txt
 cat dragon.txt
 ls -ltra
